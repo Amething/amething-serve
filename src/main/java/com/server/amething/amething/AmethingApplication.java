d0b4e1cb1ce1b6c8eaa24c39447d333a52762a40
YongWoo <aba1430@naver.com>
2022-04-18 16:33:21 +0900

    [ADD] Default Setting

---
+++ src/main/java/com/server/amething/amething/AmethingApplication.java
@@ -0,0 +1,13 @@
+package com.server.amething.amething;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmethingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmethingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/amething/amething/AmethingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.amething.amething;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmethingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
