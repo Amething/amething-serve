b926dd6d0cd5f3f996c008ee670575a0f7a79e3c
taem1n1 <youman7953@naver.com>
2022-04-19 14:03:34 +0900

    Amething 전역에서 반환되는 Exception들과 Dto제약조건에 따라 Exception을 반환해주는 Exception을 처리하는 Handler

--- src/main/java/com/server/amething/global/exception/handler/AmethingExceptionHandler.java
+++ src/main/java/com/server/amething/global/exception/handler/AmethingExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.server.amething.global.exception.handler;
 
-import com.server.Dotori.global.exception.DotoriException;
-import com.server.Dotori.global.exception.ErrorResponse;
+import com.server.amething.global.exception.AmethingException;
+import com.server.amething.global.exception.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,17 +13,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
-public class DotoriExceptionHandler {
+public class AmethingExceptionHandler {
 
-    @ExceptionHandler(value = {DotoriException.class})
-    public ResponseEntity<ErrorResponse> handleDotoriException(DotoriException e) {
-        log.error("handleDotoriException throw DotoriException : {}", e.getErrorCode());
+    @ExceptionHandler(value = {AmethingException.class})
+    public ResponseEntity<ErrorResponse> handleDotoriException(AmethingException e) {
+        log.error("HandleAmethingException throw AmethingException : {}", e.getErrorCode());
         return ErrorResponse.toResponseEntity(e.getErrorCode());
     }
 
     @ExceptionHandler(value = {MethodArgumentNotValidException.class})
     public ResponseEntity<ErrorResponse> handleInvalidRequestDataException(MethodArgumentNotValidException e){
-        log.error("handleInvalidRequestDataException throw MethodArgumentNotValidException");
+        log.error("HandleInvalidRequestDataException throw MethodArgumentNotValidException");
         BindingResult result = e.getBindingResult();
         StringBuilder builder = new StringBuilder();
 
