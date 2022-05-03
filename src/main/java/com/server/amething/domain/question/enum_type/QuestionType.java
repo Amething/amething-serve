ab684711178431fdc917fc7b04afb138fd40ae1a
taem1n1 <youman7953@naver.com>
2022-05-03 22:42:23 +0900

    질문의 타입 enum으로 관리

--- src/main/java/com/server/amething/domain/question/enum_type/QuestionType.java
+++ src/main/java/com/server/amething/domain/question/enum_type/QuestionType.java
@@ -1,2 +1,10 @@
-package com.server.amething.domain.question.enum_type;public class Question t{
+package com.server.amething.domain.question.enum_type;
+
+/**
+ * PIN: 고정된 질문
+ * BASIC: 일반 질문
+ * UNCOMMENT: 답변하지 않은 질문
+ */
+public enum QuestionType {
+    PIN, BASIC, UNCOMMENT
 }
