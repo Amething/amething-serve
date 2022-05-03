892dcafcc741d05b4e38263d9642f62973a7c7dc
taem1n1 <youman7953@naver.com>
2022-05-03 22:41:55 +0900

    멤버 테이블 설계

--- src/main/java/com/server/amething/domain/member/Member.java
+++ src/main/java/com/server/amething/domain/member/Member.java
@@ -1,4 +1,34 @@
 package com.server.amething.domain.member;
 
+import lombok.*;
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+
+@Entity @Table(name = "member")
+@Getter @Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "member_nickname")
+    private String nickname;
+
+    @Column(name = "member_password")
+    private String password;
+
+    @Column(name = "member_name")
+    private String memberName;
+
+    @Column(name = "member_bio")
+    private String bio;
+
+    @Column(name = "member_profile_picture")
+    private String profilePicture;
+
 }
+
