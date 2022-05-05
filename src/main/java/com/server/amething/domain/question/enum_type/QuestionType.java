aeebbca5977ee48ed58b3147f6584396cc5e5548
taem1n1 <youman7953@naver.com>
2022-05-05 22:01:02 +0900

    EnumType 필드 변경

--- src/main/java/com/server/amething/domain/question/enum_type/QuestionType.java
+++ src/main/java/com/server/amething/domain/question/enum_type/QuestionType.java
@@ -2,9 +2,8 @@ package com.server.amething.domain.question.enum_type;
 
 /**
  * PIN: 고정된 질문
- * BASIC: 일반 질문
- * UNCOMMENT: 답변하지 않은 질문
+ * UNREPLY: 답변하지 않은 질문
  */
 public enum QuestionType {
-    PIN, BASIC, UNCOMMENT
+    PIN, UNREPLY
 }
