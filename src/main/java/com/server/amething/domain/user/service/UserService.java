8be4385c985751ee33d43a175da49639d7c07ea5
taem1n1 <youman7953@naver.com>
2022-05-21 16:44:17 +0900

    유저 정보를 저장하기 위한 서비스 state

--- src/main/java/com/server/amething/domain/user/service/UserService.java
+++ src/main/java/com/server/amething/domain/user/service/UserService.java
@@ -1,2 +1,7 @@
-package com.server.amething.domain.user.service;public interface UserService {
+package com.server.amething.domain.user.service;
+
+import com.server.amething.domain.auth.dto.UserProfileResponseDto;
+
+public interface UserService {
+    void saveUserInfo(UserProfileResponseDto userProfileResponseDto);
 }
--- src/main/java/com/server/amething/domain/user/service/UserServiceImpl.java
+++ src/main/java/com/server/amething/domain/user/service/UserServiceImpl.java
@@ -1,2 +1,23 @@
-package com.server.amething.domain.user.service;public class UserServiceImpl {
+package com.server.amething.domain.user.service;
+
+import com.server.amething.domain.auth.dto.UserProfileResponseDto;
+import com.server.amething.domain.user.User;
+import com.server.amething.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    public void saveUserInfo(UserProfileResponseDto userProfileResponseDto) {
+        userRepository.save(User.builder()
+                .userName(userProfileResponseDto.getProperties().getNickname())
+                .profilePicture(userProfileResponseDto.getProperties().getProfile_image())
+                .build()
+        );
+    }
 }
