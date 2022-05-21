b4dd98d53b2fa3299428a4b09052ad7c21023129
taem1n1 <youman7953@naver.com>
2022-05-21 16:44:49 +0900

    User의 Profile 정보들을 받아오는 Dto

--- src/main/java/com/server/amething/domain/auth/dto/UserProfileResponseDto.java
+++ src/main/java/com/server/amething/domain/auth/dto/UserProfileResponseDto.java
@@ -1,2 +1,47 @@
-package com.server.amething.domain.auth.dto;public class UserProfileResponseDto {
+package com.server.amething.domain.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserProfileResponseDto {
+    private long id;
+    private String connected_at;
+    private Properties properties;
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class Properties {
+        private String nickname;
+        private String profile_image;
+        private String thumbnail_image;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class KakaoAcount {
+        public Boolean profile_needs_agreement;
+        public Profile profile;
+        public Boolean has_email;
+        public Boolean email_needs_agreement;
+        public Boolean is_email_valid;
+        public Boolean is_email_verified;
+        public String email;
+
+        @Getter
+        @NoArgsConstructor
+        @AllArgsConstructor
+        public class Profile {
+            public String nickname;
+            public String thumbnail_image_url;
+            public String profile_image_url;
+            public boolean is_default_image;
+        }
+    }
+
 }
