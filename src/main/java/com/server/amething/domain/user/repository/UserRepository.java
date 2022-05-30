5f1425bdd8e93abe61fd63ccc97f388cf818e00b
YongWoo <aba1430@naver.com>
2022-05-30 20:41:09 +0900

    [ADD] Username으로 유저정보를 가져오는 Repo

--- src/main/java/com/server/amething/domain/user/repository/UserRepository.java
+++ src/main/java/com/server/amething/domain/user/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.server.amething.domain.user.repository;
 
 import com.server.amething.domain.user.User;
+import com.server.amething.domain.user.dto.ProFileDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findAllByUserName(String userName);
 }
