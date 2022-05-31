475d84d0cab63ab4a72f7d75ec1912de832382a3
YongWoo <aba1430@naver.com>
2022-05-31 12:29:33 +0900

    [ADD] 커스텀 레포 구현을 위한 Impl 생성

---
+++ src/main/java/com/server/amething/domain/user/repository/UserRepositoryImpl.java
@@ -0,0 +1,13 @@
+package com.server.amething.domain.user.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.server.amething.domain.user.dto.ProfileDto;
+import lombok.RequiredArgsConstructor;
+
+import static com.server.amething.domain.user.QUser.user;
+
+@RequiredArgsConstructor
+public class UserRepositoryImpl implements UserRepositoryCustom{
+
+}
