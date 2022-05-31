08f4a2cc55cd68811e188c97c720b8ce104814e5
YongWoo <aba1430@naver.com>
2022-05-31 12:29:01 +0900

    [ADD] findProfileByUsername 메소드 추가

--- src/main/java/com/server/amething/domain/user/repository/UserRepositoryCustom.java
+++ src/main/java/com/server/amething/domain/user/repository/UserRepositoryCustom.java
@@ -3,4 +3,5 @@ package com.server.amething.domain.user.repository;
 import com.server.amething.domain.user.dto.ProfileDto;
 
 public interface UserRepositoryCustom {
+    ProfileDto findProfileByUsername(String username);
 }
