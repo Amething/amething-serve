6c423f7aa7738e81843c8df5c1aa25c6110ac870
YongWoo <aba1430@naver.com>
2022-05-31 13:43:04 +0900

    [ADD] extends 추가 및 기존에 사용하던 로직 삭제

--- src/main/java/com/server/amething/domain/user/repository/UserRepository.java
+++ src/main/java/com/server/amething/domain/user/repository/UserRepository.java
@@ -2,7 +2,8 @@ package com.server.amething.domain.user.repository;
 
 import com.server.amething.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findAllByUserName(String userName);
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 }
