3d1754d378314dbf47702e2cd05f5743d08e56f3
taem1n1 <youman7953@naver.com>
2022-06-04 15:42:45 +0900

    Custom한 JwtFilter를 Spring Filter에 끼워주는 클래스

--- src/main/java/com/server/amething/global/jwt/config/JwtFilterConfig.java
+++ src/main/java/com/server/amething/global/jwt/config/JwtFilterConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
-public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+public class JwtFilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
     private final JwtTokenProvider jwtTokenProvider;
 
@@ -19,4 +19,3 @@ public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurity
         http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
     }
 }
-ㅋ
