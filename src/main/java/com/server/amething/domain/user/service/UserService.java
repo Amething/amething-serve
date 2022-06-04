9c6b9c2361a572c08cf6a2d0b73aa4b67a18968b
taem1n1 <youman7953@naver.com>
2022-06-04 15:46:57 +0900

    로그인 로직 변경

--- src/main/java/com/server/amething/domain/user/service/UserService.java
+++ src/main/java/com/server/amething/domain/user/service/UserService.java
@@ -1,7 +1,11 @@
 package com.server.amething.domain.user.service;
 
 import com.server.amething.domain.auth.dto.UserProfileResponseDto;
+import com.server.amething.domain.user.enum_type.Role;
+
+import java.util.List;
+import java.util.Map;
 
 public interface UserService {
-    void saveUserInfo(UserProfileResponseDto userProfileResponseDto);
+    Map<String, String> login(UserProfileResponseDto userProfileResponseDto, List<Role> roles);
 }
