414686da2d6e80f218577fc8ccf77131674b94e3
YongWoo <aba1430@naver.com>
2022-06-09 20:23:46 +0900

    [ADD] bio 컬럼을 가져오는 메소드 추가 및 Transactional 어노테이션 사용

--- src/main/java/com/server/amething/domain/user/repository/UserRepositoryImpl.java
+++ src/main/java/com/server/amething/domain/user/repository/UserRepositoryImpl.java
@@ -4,6 +4,8 @@ import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.server.amething.domain.user.dto.ProfileDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.Optional;
 
 import static com.server.amething.domain.user.QUser.user;
@@ -14,12 +16,13 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public ProfileDto findProfileByOauthId(Long oauthId) {
+    @Transactional(readOnly = true)
     public Optional<ProfileDto> findProfileByOauthId(Long oauthId) {
         return Optional.ofNullable(queryFactory.from(user)
                 .select(Projections.constructor(ProfileDto.class,
                         user.nickname,
                         user.profilePicture,
+                        user.bio
                         ))
                 .where(user.oauthId.eq(oauthId))
                 .fetchOne());
