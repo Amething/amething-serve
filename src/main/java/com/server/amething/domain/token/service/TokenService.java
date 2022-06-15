799cb596415e3c16f400ebd956116ff01b4413a4
taem1n1 <youman7953@naver.com>
2022-06-15 09:29:51 +0900

    AccessToken 재발급 로직 생성

--- src/main/java/com/server/amething/domain/token/service/TokenService.java
+++ src/main/java/com/server/amething/domain/token/service/TokenService.java
@@ -1,2 +1,8 @@
-package com.server.amething.domain.token.service;public interface TokenService {
+package com.server.amething.domain.token.service;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface TokenService {
+
+    String reissueAccessToken(HttpServletRequest request, Long oauthId);
 }
