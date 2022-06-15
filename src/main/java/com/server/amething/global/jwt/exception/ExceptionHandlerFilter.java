764a0624a040a9d54cb5ad9c66b3a4bf03f9a4c2
taem1n1 <youman7953@naver.com>
2022-06-15 09:29:00 +0900

    JWT를 검증하고 요청받은 JWT의 예외를 던져주는 Handler

--- src/main/java/com/server/amething/global/jwt/exception/ExceptionHandlerFilter.java
+++ src/main/java/com/server/amething/global/jwt/exception/ExceptionHandlerFilter.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.server.amething.global.exception.response.ErrorResponse;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -19,33 +21,40 @@ import java.io.IOException;
 @Component
 @Slf4j
 @RequiredArgsConstructor
-public class ExceptionHandleFilter extends OncePerRequestFilter {
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
     private final ObjectMapper objectMapper;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException {
         try {
             filterChain.doFilter(request, response);
         } catch (ExpiredJwtException ex) {
             log.debug("================= [ ExceptionHandlerFilter ] 에서 ExpiredJwtException 발생 ===================");
-            setErrorResponse(HttpStatus.UNAUTHORIZED, response);
+            setErrorResponse(HttpStatus.UNAUTHORIZED, response, "token의 유효기간이 만료 되었습니다.");
+        } catch (SignatureException ex) {
+            log.debug("================= [ ExceptionHandlerFilter ] 에서 SignatureException 발생 ===================");
+            setErrorResponse(HttpStatus.UNAUTHORIZED, response, "token의 secretKey가 변질 되었습니다.");
+        } catch (MalformedJwtException ex) {
+            log.debug("================= [ ExceptionHandlerFilter ] 에서 MalformedJwtException 발생 ===================");
+            setErrorResponse(HttpStatus.UNAUTHORIZED, response, "token이 위조 되었습니다.");
         } catch (JwtException | IllegalArgumentException ex) {
             log.debug("================= [ ExceptionHandlerFilter ] 에서 JwtException 발생 ===================");
-            setErrorResponse(HttpStatus.FORBIDDEN, response);
+            setErrorResponse(HttpStatus.FORBIDDEN, response, "요청에 사용된 token이 올바르지 않습니다.");
         } catch (Exception ex) {
             log.debug("================= [ ExceptionHandlerFilter ] 에서 Exception 발생 ===================");
-            setErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, response);
+            setErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, response, "알 수 없는 SERVER 에러가 발생하였습니다.");
         }
     }
 
-    public void setErrorResponse(HttpStatus httpStatus, HttpServletResponse response) throws IOException {
+    public void setErrorResponse(HttpStatus httpStatus, HttpServletResponse response, String message) throws IOException {
         response.setStatus(httpStatus.value());
         response.setContentType("application/json; charset=utf-8");
 
         ErrorResponse errorResponse = ErrorResponse.builder()
                 .status(httpStatus.value())
                 .error(httpStatus.name())
+                .message(message)
                 .build();
         String errorResponseEntityToJson = objectMapper.writeValueAsString(errorResponse);
         response.getWriter().write(errorResponseEntityToJson.toString());
