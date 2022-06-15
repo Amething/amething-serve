6caf860b64e7bd4281397ec07de87649a67099ce
taem1n1 <youman7953@naver.com>
2022-06-15 09:30:45 +0900

    ExceptionHandler와 JwtTokenProvider를 SecurityFilter에 등록

--- src/main/java/com/server/amething/global/config/security/WebSecurityConfig.java
+++ src/main/java/com/server/amething/global/config/security/WebSecurityConfig.java
@@ -1,6 +1,15 @@
 package com.server.amething.global.config.security;
 
+import com.server.amething.global.jwt.JwtTokenProvider;
+import com.server.amething.global.jwt.config.JwtFilterConfig;
+import com.server.amething.global.jwt.exception.ExceptionHandlerFilter;
+//import com.server.amething.global.jwt.exception.ExceptionHandlerFilterConfig;
+import com.server.amething.global.jwt.exception.ExceptionHandlerFilterConfig;
+import com.server.amething.global.jwt.exception.JwtAccessDeniedHandler;
+import com.server.amething.global.jwt.exception.JwtAuthenticationEntryPoint;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,8 +18,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity //(debug = true)
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
@@ -22,14 +34,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.authorizeRequests()
-                .antMatchers("/**").permitAll()
+//                .antMatchers("/v1/user/**").hasRole("GUEST")
                 .anyRequest().permitAll();
+
+        http.exceptionHandling()
+                .accessDeniedHandler(new JwtAccessDeniedHandler())
+                .authenticationEntryPoint(new JwtAuthenticationEntryPoint());
+
+        http.apply(new JwtFilterConfig(jwtTokenProvider));
+        http.apply(new ExceptionHandlerFilterConfig(exceptionHandlerFilter));
     }
 
     @Override
     public void configure(WebSecurity web) {
         web.ignoring()
-                .antMatchers("/**") // 후에 Security 설정을 완료하고 제거
+//                .antMatchers("/**") // 후에 Security 설정을 완료하고 제거
                 .antMatchers("/h2-console/**", "/exception/**")
                 .antMatchers("/swagger-resources/**")
                 .antMatchers("/swagger-ui.html")
@@ -39,4 +58,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/configuration/security")
                 .antMatchers("/context/**");
     }
+
 }
