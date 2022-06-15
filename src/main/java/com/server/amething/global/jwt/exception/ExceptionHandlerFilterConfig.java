d0df7bb4d01f9fe7686764cc71fec54b11c109b5
taem1n1 <youman7953@naver.com>
2022-06-15 09:31:13 +0900

    생성한 사용자 정의 ExceptionHandler를 Security

--- src/main/java/com/server/amething/global/jwt/exception/ExceptionHandlerFilterConfig.java
+++ src/main/java/com/server/amething/global/jwt/exception/ExceptionHandlerFilterConfig.java
@@ -1,2 +1,20 @@
-package com.server.amething.global.jwt.exception;public class ExceptionHandlerFilterConfig {
+package com.server.amething.global.jwt.exception;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.context.SecurityContextPersistenceFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class ExceptionHandlerFilterConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final ExceptionHandlerFilter exceptionHandlerFilter;
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.addFilterBefore(exceptionHandlerFilter, SecurityContextPersistenceFilter.class);
+    }
 }
