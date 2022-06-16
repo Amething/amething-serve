d628f76138f6af96a5b1d64b1ec9a4315f27ef46
YongWoo <aba1430@naver.com>
2022-06-16 22:52:11 +0900

    [ADD] Querydsl 로직구현

---
+++ src/main/java/com/server/amething/domain/question/repository/QuestionRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.server.amething.domain.question.repository;
+
+import com.server.amething.domain.question.dto.QuestionDto;
+import com.server.amething.domain.user.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QuestionRepositoryCustom{
+    Optional<List<QuestionDto>> findAllDescriptionByUser (User user);
+}
