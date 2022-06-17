f0ee89d3024f6c50865d7150296ed68727c989e3
YongWoo <aba1430@naver.com>
2022-06-17 15:26:14 +0900

    [ADD] Join 로직 추가

---
+++ src/main/java/com/server/amething/domain/question/repository/QuestionRepositoryCustom.java
@@ -0,0 +1,13 @@
+package com.server.amething.domain.question.repository;
+
+import com.server.amething.domain.question.dto.QuestionAndAnswerDto;
+import com.server.amething.domain.question.dto.QuestionDto;
+import com.server.amething.domain.user.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface QuestionRepositoryCustom{
+    Optional<List<QuestionDto>> findUnreplyDescriptionByUser(User user);
+    Optional<List<QuestionAndAnswerDto>> findPinDescriptionByUser(User user);
+}
