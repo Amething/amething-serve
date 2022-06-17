4d81bf0994be72d12e31077fdd60d7f3c96da03d
YongWoo <aba1430@naver.com>
2022-06-17 15:17:46 +0900

    [ADD] QuestionAndAnswerDto

---
+++ src/main/java/com/server/amething/domain/question/dto/QuestionAndAnswerDto.java
@@ -0,0 +1,15 @@
+package com.server.amething.domain.question.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestionAndAnswerDto {
+
+    private String description;
+
+    private String title;
+}
