5b96d6c8925777108762b6f2945278434ecc7dcf
YongWoo <aba1430@naver.com>
2022-06-17 15:15:25 +0900

    [ADD] Answer description -> title

--- src/main/java/com/server/amething/domain/answer/Answer.java
+++ src/main/java/com/server/amething/domain/answer/Answer.java
@@ -19,8 +19,8 @@ public class Answer {
     @JoinColumn(name = "question_id")
     private Question question;
 
-    @Column(name = "answer_description")
-    private String description;
+    @Column(name = "question_title")
+    private String title;
 
 }
 
