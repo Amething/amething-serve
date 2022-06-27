d2f9cf3ccd6a5b619979c8c1b10f18ea919ab074
YongWoo <aba1430@naver.com>
2022-06-27 19:46:46 +0900

    [FIX] QuestionService 전체 Test 로직 수정

--- src/test/java/com/server/amething/domain/question/service/QuestionServiceTest.java
+++ src/test/java/com/server/amething/domain/question/service/QuestionServiceTest.java
@@ -1,6 +1,5 @@
 package com.server.amething.domain.question.service;
 
-import com.server.amething.domain.question.Question;
 import com.server.amething.domain.answer.Answer;
 import com.server.amething.domain.answer.repository.AnswerRepository;
 import com.server.amething.domain.question.Question;
@@ -11,18 +10,22 @@ import com.server.amething.domain.question.repository.QuestionRepository;
 import com.server.amething.domain.user.User;
 import com.server.amething.domain.user.enum_type.Role;
 import com.server.amething.domain.user.repository.UserRepository;
-import org.junit.jupiter.api.Assertions;
+import com.server.amething.global.jwt.config.CustomUserDetails;
+import com.server.amething.global.util.UserUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collections;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
@@ -36,10 +39,15 @@ class QuestionServiceTest {
     UserRepository userRepository;
     @Autowired
     AnswerRepository answerRepository;
+    @Autowired
+    UserUtil userUtil;
+    @Autowired
+    CustomUserDetails customUserDetails;
 
     @BeforeEach
     @DisplayName("질문 기능을 테스트 하기 위해 User 데이터 생성")
     void saveUser() {
+        //given
         User user = User.builder()
                 .bio("")
                 .nickname("김태민")
@@ -49,18 +57,28 @@ class QuestionServiceTest {
                 .refreshToken("Bearer refreshToken")
                 .build();
         userRepository.save(user);
+
+        // when login session 발급
+        UserDetails userDetails = customUserDetails.loadUserByUsername(user.getUsername());
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                userDetails,
+                "",
+                userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(token);
     }
 
     @Test
     @DisplayName("질문 저장 로직")
     void createQuestion() {
         //given
-        User user = userRepository.findByOauthId(2249049915L)
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원은 존재하지 않습니다."));
+        User user = userUtil.getCurrentUser();
+
         List<QuestionDto> questions;
         QuestionDto questionDto = new QuestionDto("개발자를 시작하시게 된 경위가 무엇인가요?");
         //when
-        questionService.createQuestion(2249049915L,questionDto);
+        questionService.createQuestion(user.getOauthId(),questionDto);
         questions = questionRepository.findUnReplyDescriptionByUser(user);
         //then
         assertEquals(questions.get(0).getTitle(), "개발자를 시작하시게 된 경위가 무엇인가요?");
@@ -70,8 +88,7 @@ class QuestionServiceTest {
     @DisplayName("저장된 미답변 질문을 가져오는 로직")
     void findUnReplyQuestion() {
         //given
-        User user = userRepository.findByOauthId(2249049915L)
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원은 존재하지 않습니다."));        
+        User user = userUtil.getCurrentUser();
         List<QuestionDto> questions;
         Question question = Question.builder()
                 .id(1L)
@@ -85,8 +102,8 @@ class QuestionServiceTest {
 
         //when
         questionRepository.save(question);
-        questionService.createQuestion(2249049915L,questionDto2);
-        questionService.createQuestion(2249049915L,questionDto3);
+        questionService.createQuestion(user.getOauthId(),questionDto2);
+        questionService.createQuestion(user.getOauthId(),questionDto3);
         questions = questionRepository.findUnReplyDescriptionByUser(user);
         //then
         questions.forEach(questionDto -> System.out.println(questionDto.getTitle()));
@@ -95,8 +112,7 @@ class QuestionServiceTest {
     @Test
     void findPinQuestion() {
         //given
-        User user = userRepository.findByOauthId(2249049915L)
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원은 존재하지 않습니다."));        
+        User user = userUtil.getCurrentUser();
         List<QuestionAndAnswerDto> questions;
 
         Question question = Question.builder()
